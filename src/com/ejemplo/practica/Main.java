00592b5c7a44e01ff910eeaa7858e6f0cb795eb9
Carlos Martínez Del Pino <carlosmartinezalixar@gmail.com>
2026-01-16 12:13:42 +0100

    Segundo commit: Modificado mensaje de salida

--- src/com/ejemplo/practica/Main.java
+++ src/com/ejemplo/practica/Main.java
@@ -1,5 +1,9 @@
 package com.ejemplo.practica;
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("Carlos Martinez Del Pino.");
+		System.out.println("Proyecto Inicial funcionando correctamente");
+		
+	}
 }
